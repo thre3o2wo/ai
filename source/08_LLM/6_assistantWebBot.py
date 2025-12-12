fc1b4632ee39162643281dd655a73b12ab832c43
thre3o2wo <thre3o2wo@gmail.com>
2025-12-12 17:58:42 +0900

    OpenAI API Assistants, FunctionCalling

---
+++ source/08_LLM/6_assistantWebBot.py
@@ -0,0 +1,89 @@
+# 인터프리터 선택 - ctrl+shift+p 
+# cmd - ctrl+j
+# 실행 - streamlit run 6_assistantWebBot.py
+# 종료 - ctrl+c 
+
+import streamlit as st
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+import time
+import os
+from dotenv import load_dotenv
+from openai import OpenAI, OpenAIError
+
+def main():
+    load_dotenv()
+    st.title('고객 지원 챗봇')
+    # st.text(os.getenv('OPENAI_API_KEY'))
+    # client 생성
+    client = OpenAI()
+    # assistant와 thred 초기화(각 id 세션에 추가)
+    if 'assistant_id' not in st.session_state:
+        assistant = client.beta.assistants.create(
+            name = 'CustomerServiceBot',
+            instructions='당신은 고객지원 챗봇입니다. 사용자 문의에 간략히 대답해 주세요.',
+            model = 'gpt-4o-mini'
+        )
+        st.session_state.assistant_id = assistant.id
+    if 'thread_id' not in st.session_state:
+        thread = client.beta.threads.create()
+        st.session_state.thread_id = thread.id
+    # 대화 이력 초기화
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+    # 대화 표시
+    for msg in st.session_state.messages:
+        st.chat_message(msg['role']).write(msg['content'])
+    # 사용자 입력 받기
+    if prompt := st.chat_input("메세지를 입력하세요"):
+        prompt = prompt.strip()
+        # 사용자 메세지를 session 추가, 화면 출력
+        st.session_state.messages.append({"role":"user", "content":prompt})
+        st.chat_message("user").write(prompt)
+        # 사용자 메세지를 thread에 추가
+        client.beta.threads.messages.create(
+            thread_id = st.session_state.thread_id,
+            role = "user",
+            content = prompt
+        )
+        # 실행(답변 요청) - 과금 
+        client.beta.threads.runs.create_and_poll(
+            thread_id=st.session_state.thread_id,
+            assistant_id=st.session_state.assistant_id
+        )
+        # 최신 답변 가져오기
+        messages = client.beta.threads.messages.list(
+            thread_id=st.session_state.thread_id
+        )
+        reply = messages.data[0].content[0].text.value
+        # 답변을 session에 추가하고 화면 출력
+        st.session_state.messages.append(
+            {'role':'assistant', 'content':reply}
+        )
+        st.chat_message('assistant').write(reply)
+    # 대화 이력 백업
+    if st.button('대화 이력 백업'):
+        # thread에서 전체 메시지 list로 가져오기
+        messages = client.beta.threads.messages.list(thread_id=st.session_state.thread_id)
+        if messages.data:
+            # messages가 빈 리스트가 아니면, 시간 순서대로 정렬한 후 파일 백업
+            sorted_messages = sorted(
+                messages.data,
+                key=lambda msg : msg.created_at
+            )
+            with open('data/ch7_history.txt', 'w', encoding='utf-8') as f:
+                for msg in sorted_messages:
+                    # role / content / dateStr
+                    role = msg.role
+                    content = msg.content[0].text.value
+                    dateStr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(msg.created_at))
+                    # 파일 기록
+                    row = "{:9}({}) : {}\n".format(role, dateStr, content)
+                    f.write(row)
+                st.info('대화 내역을 저장하였습니다.')
+        else:
+            st.warning('저장할 대화 내역이 없습니다.')
+    st.caption('대화 수 : {}'.format(len(st.session_state.messages)))
+
+if __name__=='__main__':
+    main()
